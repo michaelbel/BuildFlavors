5710a32b45cf2767866925d8173c178c482c55ad
michaelbel <michael-bel@outlook.com>
2025-03-10 18:28:41 +0300

    Update project

--- app/src/main/kotlin/org/michaelbel/applanguage/MainActivity.kt
+++ app/src/main/kotlin/org/michaelbel/applanguage/MainActivity.kt
@@ -1,9 +1,24 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package org.michaelbel.applanguage
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import org.michaelbel.applanguage.ui.AppTheme
 
@@ -15,7 +30,33 @@ class MainActivity: ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AppTheme {
+                val context = LocalContext.current
 
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(
+                            title = {
+                                Text(
+                                    text = stringResource(R.string.title_language)
+                                )
+                            }
+                        )
+                    }
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .padding(horizontal = 16.dp)
+                            .fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(
+                            text = stringResource(R.string.greeting)
+                        )
+                    }
+                }
             }
         }
     }
