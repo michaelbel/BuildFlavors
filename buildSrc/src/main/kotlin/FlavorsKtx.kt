a9910895102662a907e2878eaab433009be4da72
michaelbel <michael-bel@outlook.com>
2025-09-29 19:37:51 +0300

    Update project

--- buildSrc/src/main/kotlin/FlavorsKtx.kt
+++ buildSrc/src/main/kotlin/FlavorsKtx.kt
@@ -3,13 +3,13 @@ import org.gradle.api.Project
 val Project.isGmsBuild: Boolean
     get() {
         val requests = gradle.startParameter.taskRequests.joinToString(" ") { it.args.joinToString(" ") }
-        val regex = Regex("(assemble|bundle|install|package|publish|test|connected|device|lint|generate).+Gms", RegexOption.IGNORE_CASE)
+        val regex = Regex("(assemble|install).+Gms", RegexOption.IGNORE_CASE)
         return regex.containsMatchIn(requests)
     }
 
 val Project.isHmsBuild: Boolean
     get() {
         val requests = gradle.startParameter.taskRequests.joinToString(" ") { it.args.joinToString(" ") }
-        val regex = Regex("(assemble|bundle|install|package|publish|test|connected|device|lint|generate).+Hms", RegexOption.IGNORE_CASE)
+        val regex = Regex("(assemble|install).+Hms", RegexOption.IGNORE_CASE)
         return regex.containsMatchIn(requests)
     }
